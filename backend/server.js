238e8b863acb5541fd29e516fa4f047de962afb0
srahman14 <s.rahman14@hotmail.com>
2025-10-17 20:15:43 +0100

    added movie routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
+
+// Routes
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
+// --- IGNORE ---
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -9,6 +14,8 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server started on PORT " + PORT)
