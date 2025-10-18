a9643ef0a3cf8673b6201a9ef32634dc59e60d74
srahman14 <s.rahman14@hotmail.com>
2025-10-18 02:12:02 +0100

    added default image on signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -46,11 +46,15 @@ export async function signup(req, res) {
 
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
+		const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+
+		const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
     const newUser = new User({
       email: email,
       username: username,
       password: hashedPassword,
+      image: image,
     });
 
     generateTokenAndSetCookie(newUser._id, res);
