87d8d729475fc9287bc62b8ff77ea8abcd0d52b2
srahman14 <s.rahman14@hotmail.com>
2025-10-22 20:35:44 +0100

    added Toaster, Footer and loading state for checking user auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,37 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/home/HomePage'
+import Footer from './components/Footer'
+import { Toaster } from 'react-hot-toast'
+import { useAuthUserStore } from './store/authUser'
+import { useEffect } from 'react'
+import { LoaderCircle } from 'lucide-react'
 
 function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthUserStore();
+  console.log("auth user is here", user);
 
+  useEffect(() => {
+    authCheck();
+  }, []);
+
+  if (isCheckingAuth) {
+    return (
+      <div className='h-screen flex justify-center items-center bg-black h-full'>
+        <LoaderCircle className='animate-spin text-red-600 size-10 mx-auto mt-20' />
+      </div>
+    )
+  }
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
+      <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
     </Routes>
+    <Footer />
+    <Toaster />
     </>
   )
 }
