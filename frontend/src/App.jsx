e4ac9831379645e271cdd8b092f79073053fb013
srahman14 <s.rahman14@hotmail.com>
2025-10-23 21:57:14 +0100

    added search history/search/404 routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import { useAuthUserStore } from './store/authUser'
 import { useEffect } from 'react'
 import { LoaderCircle } from 'lucide-react'
 import WatchPage from './pages/WatchPage'
+import SearchPage from './pages/SearchPage'
+import SearchHistoryPage from './pages/SearchHistoryPage'
+import NotFoundPage from './pages/404'
+
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthUserStore();
@@ -31,6 +35,10 @@ function App() {
       <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
       <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
       <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+      <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+      <Route path="/history" element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+      <Route path="/*" element={<NotFoundPage />} />
+
     </Routes>
     <Footer />
     <Toaster />
