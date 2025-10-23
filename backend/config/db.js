c086b08d1ea871d891b9af8e69273387dc2a5543
srahman14 <s.rahman14@hotmail.com>
2025-10-23 22:43:05 +0100

    deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,7 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
   try {
-    const connect = await mongoose
-      .connect(ENV_VARS.MONGO_URI)
-      .then(() => console.log("MongoDB Connected"))
-      .catch((err) => console.error("MongoDB Connection Failed", err));
+    const connect = await mongoose.connect(ENV_VARS.MONGO_URI)
     console.log("MongoDB connected: " + connect.connection.host);
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
